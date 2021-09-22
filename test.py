4b62cefac51731536f7125705d6facf6380a4042
kdexii <lasvacations@yandex.ru>
2021-09-22 16:04:23 +0300

    fix bug

--- test.py
+++ test.py
@@ -2,47 +2,56 @@
 from tkinter import *
 import tkcalendar
 from datetime import timedelta
-import json, requests
+import json
 from requests.api import get
-import telebot
-import sys
+import locale
+from tkinter import messagebox
+
 token = '1943394345:AAGOULIQSLKuGSW6NKUyhIdExkYcLrQFpy8'
-chat_id = -579353316
+chat_id = -599179418
 question = 'Ретро (Время МСК)'
 telegram_poll_url = f'https://api.telegram.org/bot{token}/sendPoll?'
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
 
 main = Tk()
+main.geometry("750x450")
 
 def getDateRange(start,stop):
-    global datesDirt
+    
     datesDirt = []
     tkdate = (stop-start).days
     for i in range(tkdate+1):
         day = start + timedelta(days=i)
         datesDirt.append(day)
-        
+    lenDate = len(datesDirt)
     if datesDirt:
         
-        datesDirt = [x.strftime('%Y-%m-%d') for x in datesDirt]
+        datesDirt = [x.strftime('%A %d-%m') for x in datesDirt]
         a = len(datesDirt)
         count = - 1
         listdate = []
         while count <= int(a) - 2:
             count = count + 1
-            # print(dates[count] + ' 10:00 - 13:00') # if u want see this trash = uncomment
             listdate.append(datesDirt[count] + ' 10:00 - 13:00')
-            # print(dates[count] + ' 13:00 - 16:00')
             listdate.append(datesDirt[count] + ' 13:00 - 16:00')
         quesUrl = f'{telegram_poll_url}chat_id={chat_id}&'\
         f'question={question}&options={json.dumps(listdate)}'\
         f'&is_anonymous=false'
-        get(quesUrl)
-        # print(quesUrl)
-        
+
+        if int(lenDate) > 5:
+            messagebox.showinfo("Error!","The maximum range of the pool is 5")
+            pass
+        else:
+            
+            get(quesUrl)
+            messagebox.showinfo("Success!","Poll send to group!")
+            pass
 
     else:
-        print('Date pool is incorrect')
-    sys.exit()
+        # print('Date pool is incorrect')
+        messagebox.showinfo("Error","Date pool is incorrect")
+        pass
+    # sys.exit()
 firstDate = tkcalendar.DateEntry(main)
 firstDate.pack(padx=10,pady=10)
 
